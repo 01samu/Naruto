882793b5940570a65d5392e762983eb87c85254b
Samuel <99812096+01samu@users.noreply.github.com>
2022-06-19 13:02:56 -0300

    Add files via upload

---
+++ p.js
@@ -0,0 +1,18 @@
+alert("Olá mundo");
+var personagens, nomes, msg;
+personagem=prompt("digite seu personagem Favorito");
+var nomes = ["Naruto", "Sasuke", "itachi", "Madara", "Minato"];
+personagens=parseFloat(personagens);
+
+
+
+
+
+
+
+
+
+
+//prompt("Qual é seu personagem favorito ?")
+
+
